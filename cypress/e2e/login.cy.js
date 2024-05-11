7fe4bd299af8c96a606331a29a3d07f4977dc69a
Prof. Angelo F. Dias <prof2132@iesp.edu.br>
2024-05-10 21:17:43 -0300

    Trabalho com fixtures e testes com forEach.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,9 +9,29 @@ context('Login do Saucedemo', () => {
         cy.get('[data-test="username"]').type('standard_user')
         cy.get('[data-test="password"]').type('secret_sauce')
         cy.get('#login-button').click()
-        cy.get('.product_label').should('contain','Produto')
-
+        cy.get('.product_label').contains('Products')
     });
 
+    it('Falhas no login', () => {
+
+        cy.fixture('users_fail.json').then((dados) => {
+
+            // const usuarioInvalido = dados.usuarios[5]
+
+            dados.usuarios.forEach((usuario) => {
+                cy.get('[data-test="username"]').clear()
+                cy.get('[data-test="password"]').clear()
+                if (usuario.usuario != '') {
+                    cy.get('[data-test="username"]').type(usuario.usuario)
+                }
+                if (usuario.senha != '') {
+                    cy.get('[data-test="password"]').type(usuario.senha)  
+                }
+    
+                cy.get('#login-button').click()
+                cy.get('[data-test="error"]').should('have.text',usuario.mensagem) 
+            });
+        })
+    });
 
 })
